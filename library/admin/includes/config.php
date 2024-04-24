3b30c1df558b04be4578f21325c5780e952d177b
VidyaKhavare <158164146+VidyaKhavare@users.noreply.github.com>
2024-04-24 02:35:23 -0700

    Update config.php

--- library/admin/includes/config.php
+++ library/admin/includes/config.php
@@ -2,7 +2,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','FJ4Ark5Watn+');
 define('DB_NAME','library');
 // Establish database connection.
 try
